285566a365ce34057cf228404e2dfc0b74ee6387
nTraum <philipp.press@googlemail.com>
2012-11-13 01:34:59 +0100

    working version

---
+++ README.md
@@ -0,0 +1,22 @@
+# tfwarsbot
+A Simple IRC bot saying things every once in a while.
+
+## Installation
+
+Clone the git repository:
+
+    $ git://github.com/nTraum/tfwarsbot.git
+
+Install necessary gems:
+
+    $ bundle
+
+## Usage
+
+Edit the configuration file `config.yml`:
+
+    $ vim config.yml
+
+Start the bot:
+
+    $ ruby tfwarsbot.rb
--- tfwarsbot.rb
+++ tfwarsbot.rb
@@ -5,29 +5,47 @@ require 'yaml'
 
 CONFIG_FILE = 'config.yml'
 
-def load_config_file
-  abort "File not found: #{File.absolute_path(CONFIG_FILE)}" unless File.readable?(CONFIG_FILE)
-  yml = YAML.load_file(CONFIG_FILE)
+def load_config_file(file)
+  abort "File not found: #{File.absolute_path(file)}" unless File.readable?(file)
+  yml = YAML.load_file(file)
   cfg = OpenStruct.new
-  cfg.server = yml['server']
-  cfg.port = yml['port']
-  cfg.channel = yml['channel']
-  cfg.nickname = yml['nickname']
-  cfg.messages = yml['messages']
+  cfg.server            = yml['server']
+  cfg.port              = yml['port']
+  cfg.channel           = yml['channel']
+  cfg.nickname          = yml['nickname']
+  cfg.verbose           = yml['verbose']
+  cfg.messages          = yml['messages']
+  cfg.interval          = yml['interval']
+  cfg.sleep_hour_begin  = yml['sleep_hour_begin']
+  cfg.sleep_hour_end    = yml['sleep_hour_end']
   cfg
 end
 
-cfg = load_config_file
+def start_bot(cfg)
+  bot = Cinch::Bot.new do
+    configure do |c|
+      c.nick            = cfg.nickname
+      c.realname        = cfg.nickname
+      c.server          = cfg.server
+      c.port            = cfg.port
+      c.channels        = [] << cfg.channel
+      c.verbose         = cfg.verbose
+    end
 
-bot = Cinch::Bot.new do
-  configure do |c|
-    c.nick            = cfg.nickname
-    c.server          = cfg.server
-    c.port            = cfg.port
-    c.channels        = cfg.channel
-    c.verbose         = true
-    c.plugins.plugins = [AnnouncePlugin]
+    on :connect do |c|
+      Timer(cfg.interval) do
+        t = Time.now
+        sleep_begin = Time.local(t.year, t.month, t.day, cfg.sleep_hour_begin)
+        sleep_end = Time.local(t.year, t.month, t.day, cfg.sleep_hour_end) 
+
+        Channel(cfg.channel).send(Format(:bold, 'Tip: ') + Format(:reset, :yellow, cfg.messages.sample)) unless t.between?(sleep_begin, sleep_end)
+        debug 'Skipping message, sleeping time...' if t.between?(sleep_begin, sleep_end)
+      end
+    end
   end
+
+  bot.start
 end
 
-bot.start
+cfg = load_config_file(CONFIG_FILE)
+start_bot cfg
